5585e120dc4552afc172c83a8eee6d5f7e51e8f2
Hakim Bohra <hakbohra@gmail.com>
2026-04-28 18:15:08 +0530

    Dummy change to trigger github action

--- EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
+++ EssentialFeedAPIEndToEndTests/EssentialFeedAPIEndToEndTests.swift
@@ -25,7 +25,7 @@ final class EssentialFeedAPIEndToEndTests: XCTestCase {
         case let .failure(error)?:
             XCTFail("Expected successful feed result, got \(error) instead.")
         default:
-            XCTFail("Expected successful feed result, got no result instead")
+            XCTFail("Expected successful feed result, got no result instead.")
         }
     }
     
